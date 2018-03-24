92fe8ad564c02ce775489445ddc256c355ef320e
acsum <37217110+acsum@users.noreply.github.com>
2018-03-24 14:58:33 +0100

    Update and rename task_1b.py to task_1b_anna.py

--- task_1b_anna.py
+++ task_1b_anna.py
@@ -15,18 +15,11 @@ y = trainfile['y']
 X=np.array(X) #900x5
 y=np.array(y)
 
-x1=X[:,0]
-x2=X[:,1]
-x3=X[:,2]
-x4=X[:,3]
-x5=X[:,4]
-
-phi=np.transpose([x1, x2, x3, x4, x5, x1**2, x2**2, x3**2, x4**2, x5**2, np.exp(x1), np.exp(x2), np.exp(x3), np.exp(x4), np.exp(x5), np.cos(x1), np.cos(x2), np.cos(x3), np.cos(x4), np.cos(x5), np.ones(900)]) #900X21
-phi2=X #900X5
-phi2=np.append(phi2,X**2,axis=1) #900X10
-phi2=np.append(phi2, np.exp(X), axis=1) #900x15
-phi2=np.append(phi2, np.cos(X), axis=1) #900X20
-phi2=np.append(phi2, np.ones((900,1)), axis=1) #900x21
+phi=X #900X5
+phi=np.append(phi2,X**2,axis=1) #900X10
+phi=np.append(phi2, np.exp(X), axis=1) #900x15
+phi=np.append(phi2, np.cos(X), axis=1) #900X20
+phi=np.append(phi2, np.ones((900,1)), axis=1) #900x21
 
 #we need to split into a test set and a train set
 #to do so we could use eg. k-fold corss validation --> often k=5 or k=10
@@ -52,12 +45,14 @@ print(RMSE)
 print(np.min(RMSE))
 indxminRMSE=np.argmin(RMSE)
 print(indxminRMSE)
+
 # output results
 d={'weight': possibleWeight[indxminRMSE]}
 output=pd.DataFrame(d)
 output.to_csv('task_1b_output.csv', index=False, header=False)
 
 
-#Insights: RMSE with linear regression too big (?) for all folds! minimum is at 9.716, index=4
+#Insights: 
+#RMSE with linear regression min(RMSE)= 9.716
 #RMSE with Ridge regression, alpha = 0.1 -> min(RMSE)=9.7154
 #RMSE wiht Lasse regression, alpha = 0.1 -->min(RMSE)=9.686
